9f5d34ea470653729066447e03cfe0c72fa0a824
Sergio Sosa <sergio.sosa@dxc.com>
2021-11-26 16:55:48 -0600

    Se creo un servicio basico

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,41 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Hola',
+      brand: 'World',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((item) => String(item.id) === id);
+    if (!coffee)
+      throw new HttpException(`Coffee ${id} not found`, HttpStatus.NOT_FOUND);
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
+  }
+
+  update(id: string, createCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+    }
+  }
+
+  remove(id: string) {
+    this.coffees = this.coffees.filter((item) => String(item.id) !== id);
+    return [];
+  }
+}
